411a0413f75811f1576fe2c3ca8cbc892c5b3cde
PNH09 <bnh0928@naver.com>
2023-09-06 21:14:54 +0900

    getStaticProps 뭐야 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,22 @@
 'use client';
 
-import { MeetupData } from "@/types/interface";
+import { MeetupData, MeetupDataList } from "@/types/interface";
 import MeetupList from "@/components/meetups/MeetupList";
-import { useEffect, useState } from "react";
-import { GetStaticProps, GetStaticPropsResult } from "next";
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { DUMMY_DATA } from "@/types/dummy-data";
 
-const DUMMY_DATA: MeetupData[] = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
-    address: "Some address 10, 1234 Some City",
-    description: "This is a second meeyup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
-    address: "Some address 10, 1234 Some City",
-    description: "This is a second meeyup!",
-  },
-  {
-    id: "m3",
-    title: "A Third Meetup",
-    image:
-      "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
-    address: "Some address 10, 1234 Some City",
-    description: "This is a second meeyup!",
-  },
-];
-
-export default function Home() {
-  
+const Home = (meetups:InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(DUMMY_DATA)
+  console.log(meetups);
   return <MeetupList meetups={DUMMY_DATA} />;
 }
 
-export const getStaticProps:GetStaticProps = () => {
+export const getStaticProps:GetStaticProps = async () => {
+  const meetups:MeetupData[] = DUMMY_DATA;
+
   return {
-    props: {
-      meetups: DUMMY_DATA
-    }
+    props: { meetups }
   }
 }
+
+export default Home;
---
+++ src/types/dummy-data.ts
@@ -0,0 +1,28 @@
+import { MeetupData } from "./interface";
+
+export const DUMMY_DATA: MeetupData[] = [
+    {
+      id: "m1",
+      title: "A First Meetup",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
+      address: "Some address 10, 1234 Some City",
+      description: "This is a second meeyup!",
+    },
+    {
+      id: "m2",
+      title: "A Second Meetup",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
+      address: "Some address 10, 1234 Some City",
+      description: "This is a second meeyup!",
+    },
+    {
+      id: "m3",
+      title: "A Third Meetup",
+      image:
+        "https://cdn.pixabay.com/photo/2017/08/07/10/22/buildings-2602324_1280.jpg",
+      address: "Some address 10, 1234 Some City",
+      description: "This is a second meeyup!",
+    },
+  ];
